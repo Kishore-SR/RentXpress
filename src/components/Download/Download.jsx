1821dfa56a75f67942777cada18940b5430930b8
Kishore S R <kishoresr01@gmail.com>
2024-11-18 20:39:52 -0500

    add: Car animation

--- src/components/Download/Download.jsx
+++ src/components/Download/Download.jsx
@@ -1,16 +1,38 @@
+import { useEffect } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import "./Download.css";
 import appleImage from "../../assets/Apple.png";
 import playStoreImage from "../../assets/PlayStore.png";
 import footerImage from "../../assets/FooterCar.png";
 
+gsap.registerPlugin(ScrollTrigger);
+
 export const Download = () => {
+  useEffect(() => {
+    const carImage = document.querySelector(".right-img img");
+
+    gsap.fromTo(
+      carImage,
+      { x: "100%" }, 
+      {
+        x: "0%", 
+        scrollTrigger: {
+          trigger: carImage,
+          start: "bottom bottom", 
+          end: "top top", 
+          scrub: true, 
+        },
+      }
+    );
+  }, []);
+
   return (
     <section className="download" id="contact">
       <div className="left-text">
         <div className="only-text">
-
-        <h2>Download the free RentXpress app</h2>
-        <p>for faster, easier booking and exclusive deals.</p>
+          <h2>Download the free RentXpress app</h2>
+          <p>for faster, easier booking and exclusive deals.</p>
         </div>
         <div className="app-links">
           <button className="app-btn">
